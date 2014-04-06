49f198ac4231409406242763bb94b2493e653e0f
U10216007 <qaz13679@yahoo.com>
2014-04-06 22:36:36 +0800

    數字可以出現在TextField上
    1.把TextField變小
    2.把按鍵順序改變：除鍵改在等於鍵的下面
    3.設一個Listener class和actionPerformed method
    用來寫當button按下去時會如何作用,也把作用的內容寫在此class,method裡
    4.用if-else判斷使用者輸入的按鍵是哪一個,並把他輸入的字元累加使他重複顯示兩個字元,因此需要先宣告number這個變數

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 
 public class Calculator extends JFrame {
 	
-	private JTextField jresult = new JTextField(100);
+	private JTextField jresult = new JTextField(10);
 	private JButton jone = new JButton("1");
 	private JButton jtwo = new JButton("2");
 	private JButton jthree = new JButton("3");
@@ -22,6 +22,7 @@ public class Calculator extends JFrame {
 	private JButton jdivide = new JButton("/");
 	private JButton jdot = new JButton(".");
 	private JButton jequal = new JButton("=");
+	private String number = "";
 	
 	public Calculator(){
 		
@@ -47,11 +48,68 @@ public class Calculator extends JFrame {
 		p2.add(jmutlity);
 		p2.add(jzero);
 		p2.add(jdot);
-		p2.add(jdivide);
 		p2.add(jequal);
+		p2.add(jdivide);
+		
 		add(p2,BorderLayout.CENTER);
 		
+		jone.addActionListener(new ButtonListner());
+		jtwo.addActionListener(new ButtonListner());
+		jthree.addActionListener(new ButtonListner());
+		jfour.addActionListener(new ButtonListner());
+		jfive.addActionListener(new ButtonListner());
+		jsix.addActionListener(new ButtonListner());
+		jseven.addActionListener(new ButtonListner());
+		jeight.addActionListener(new ButtonListner());
+		jnine.addActionListener(new ButtonListner());
+		jzero.addActionListener(new ButtonListner());
+		jdot.addActionListener(new ButtonListner());
+		
 	}
+	
+	class ButtonListner implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			if(e.getSource() == jone){
+				number = number + "1";
+				jresult.setText(number);
+			}else if(e.getSource() == jtwo){
+				number = number + "2";
+				jresult.setText(number);
+			}else if(e.getSource() == jthree){
+				number = number + "3";
+				jresult.setText(number);
+			}else if(e.getSource() == jfour){
+				number = number + "4";
+				jresult.setText(number);
+			}else if(e.getSource() == jfive){
+				number = number + "5";
+				jresult.setText(number);
+			}else if(e.getSource() == jsix){
+				number = number + "6";
+				jresult.setText(number);
+			}else if(e.getSource() == jseven){
+				number = number + "7";
+				jresult.setText(number);
+			}else if(e.getSource() == jeight){
+				number = number + "8";
+				jresult.setText(number);
+			}else if(e.getSource() == jnine){
+				number = number + "9";
+				jresult.setText(number);
+			}else if(e.getSource() == jzero){
+				number = number + "0";
+				jresult.setText(number);
+			}else if(e.getSource() == jdot){
+				number = number + ".";
+				jresult.setText(number);	
+				
+				
+			}
+		}
+		
+	}
+	
+	
 	
 	
 	
@@ -62,7 +120,7 @@ public class Calculator extends JFrame {
 		
 		Calculator frame = new Calculator();
 		frame.setTitle("Calculator");
-		frame.setSize(300,300);
+		frame.setSize(200,200);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -73,4 +131,11 @@ public class Calculator extends JFrame {
 	
 	
 }
+
+
+
+
+
+
+
 
